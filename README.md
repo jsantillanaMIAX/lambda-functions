4015dfee1fd4a177ecfb40c516c0244b9d4dc33b
jsantillanaMIAX <javier.santillana05e@gmail.com>
2024-05-06 13:14:42 +0200

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lambda-functions
 Lambda functions use with dockers
+
+Para s3
+
+docker build -t s3_lambda -f dockerfiles/s3_lambda/Dockerfile .
+
+
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 339712804222.dkr.ecr.eu-west-1.amazonaws.com
--- src/s3_lambda/app.py
+++ src/s3_lambda/app.py
@@ -1,7 +1,18 @@
-def handler (event, context):
-    
-    
-    
-    
-    
-    return 
+import pandas as pd
+import datetime
+
+def handler(event, context):
+
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    df = pd.read_csv(f"s3://{bucket}/{key}", sep=";")
+    df = df.loc[:, ["VALOR","FECHA","HORA", "PRECIO", "VOLUMEN", ]]
+
+    df.loc[:, "VALOR"] = df.loc[:, "VALOR"].str.rstrip()
+
+    df = df.loc[df.loc[:, "VALOR"] == "SAN", :]
+
+    fecha = datetime.datetime.now().strftime(f"{key}_%m-%d-%Y_%H-%M-%S")
+    df.to_csv(f"s3://miaxlambdasalida/{key}_{fecha}", compression="zip") 
+
